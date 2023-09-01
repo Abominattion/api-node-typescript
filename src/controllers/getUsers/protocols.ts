76392dec8ee01a05b21d96845b9b33a2bf24dbee
Adonay Douglas <adonay.douglas.profissional@gmail.com>
2023-09-01 17:59:08 -0300

    feat: connect mongodb

--- src/controllers/getUsers/protocols.ts
+++ src/controllers/getUsers/protocols.ts
@@ -1,4 +1,4 @@
-import { User } from "../../models/User";
+import { User } from "../../models/user";
 import { HttpResponse } from "../protocols";
 
 export interface IGetUsersController {
--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,25 @@ import { config } from "dotenv";
 import { GetUsersController } from "./controllers/getUsers/get-users";
 import { MongoGetUsersRepository } from "./repositories/mongo-get-users";
 
-config();
+const main = async () => {
+  config();
 
-const app = express();
+  const app = express();
 
-const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hellor world!");
-});
+  app.get("/", (req, res) => {
+    res.send("Hellor world!");
+  });
 
-app.get("/users", async (req, res) => {
-  const mongoGetUsersRepository = new MongoGetUsersRepository();
-  const getUsersController = new GetUsersController(mongoGetUsersRepository);
-  const response = await getUsersController.handle();
+  app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
+    const response = await getUsersController.handle();
 
-  res.send(response.body).status(response.statusCode);
-});
+    res.send(response.body).status(response.statusCode);
+    app.listen(port, () => console.log(`Rodando na porta ${port}`));
+  });
+};
 
-app.listen(port, () => console.log(`Rodando na porta ${port}`));
+main();

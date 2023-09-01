e46c620eaf2f86810caa12927674d7b75cc8e6d0
Adonay Douglas <adonay.douglas.profissional@gmail.com>
2023-09-01 18:32:07 -0300

    feat: add MongoCreateUserRepository

---
+++ src/repositories/mongo-create-user.ts
@@ -0,0 +1,28 @@
+import {
+  CreateUserParams,
+  ICreateUserRepository,
+} from "../controllers/createUsers/protocols";
+import { MongoClient } from "../database/mongo";
+import { User } from "../models/user";
+
+export class MongoCreateUser implements ICreateUserRepository {
+  async createUser(params: CreateUserParams): Promise<User> {
+    // Criando usuário
+    const { insertedId } = await MongoClient.db
+      .collection("users")
+      .insertOne(params);
+
+    // Bunscando usuário pra ver ser foi criado
+    const user = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .findOne({ _id: insertedId });
+
+    if (!user) {
+      throw new Error("Usuário não criado!");
+    }
+
+    const { _id, ...rest } = user;
+
+    return { id: _id.toHexString(), ...rest };
+  }
+}
